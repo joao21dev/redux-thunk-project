264d53cd27bff5bcfe9de197fc6f91885371122f
joao21dev <joao21dev@gmail.com>
2022-03-30 00:40:43 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import React from "react";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { reducer } from "./reducer";
+import logger from "redux-logger";
+import Info from "./Info";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const store = createStore(reducer, applyMiddleware(logger));
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div>
+          <Info />
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
---
+++ src/reducer.js
@@ -0,0 +1,9 @@
+const INITIAL_STATE = {
+  data: [],
+  isFetching: false,
+};
+export const reducer = (state = INITIAL_STATE, action) => {
+  if (action.type === "LOAD_DATA") {
+  }
+  return state;
+};
